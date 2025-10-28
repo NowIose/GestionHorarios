a1de79d3d6268501a0ee132643f0be23e9ccb96b
Nowa <dainerdual@gmail.com>
2025-10-27 23:15:25 -0400

    CAMBIO DEL ARCHIVO routes/app se agrego rutas par ael modulo usuario

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,17 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+
+    // Dashboard principal del admin
+    Route::get('/dashboard', fn() => inertia('Admin/Dashboard'))->name('admin.dashboard');
+
+    // Módulos de gestión
+    Route::get('/usuarios', fn() => inertia('Admin/GestionUsuarios'))->name('admin.usuarios');
+    Route::get('/docentes', fn() => inertia('Admin/GestionDocentes'))->name('admin.docentes');
+    Route::get('/bitacora', fn() => inertia('Admin/GestionBitacora'))->name('admin.bitacora');
+    Route::get('/roles', fn() => inertia('Admin/GestionRoles'))->name('admin.roles');
+    Route::get('/permisos', fn() => inertia('Admin/GestionPermisos'))->name('admin.permisos');
+});
 
 require __DIR__.'/settings.php';
